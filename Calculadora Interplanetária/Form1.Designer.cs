2446a5326be623c893073dd1a6f6cd8764da8a82
RunawayGuiga <guilhermemsbr@gmail.com>
2024-08-20 08:15:45 -0300

    Add files via upload

---
+++ Calculadora Interplanetária/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Atividade_Prática
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rb_Urano = new System.Windows.Forms.RadioButton();
+            this.rb_Jupiter = new System.Windows.Forms.RadioButton();
+            this.rb_Venus = new System.Windows.Forms.RadioButton();
+            this.rb_Saturno = new System.Windows.Forms.RadioButton();
+            this.rb_Marte = new System.Windows.Forms.RadioButton();
+            this.rb_Mercurio = new System.Windows.Forms.RadioButton();
+            this.txb_Peso = new System.Windows.Forms.TextBox();
+            this.bt_Menu = new System.Windows.Forms.Button();
+            this.bt_Calcular = new System.Windows.Forms.Button();
+            this.lb_Planeta = new System.Windows.Forms.Label();
+            this.lb_Peso = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // rb_Urano
+            // 
+            this.rb_Urano.AutoSize = true;
+            this.rb_Urano.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rb_Urano.Location = new System.Drawing.Point(229, 180);
+            this.rb_Urano.Name = "rb_Urano";
+            this.rb_Urano.Size = new System.Drawing.Size(71, 24);
+            this.rb_Urano.TabIndex = 22;
+            this.rb_Urano.TabStop = true;
+            this.rb_Urano.Text = "Urano";
+            this.rb_Urano.UseVisualStyleBackColor = true;
+            // 
+            // rb_Jupiter
+            // 
+            this.rb_Jupiter.AutoSize = true;
+            this.rb_Jupiter.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rb_Jupiter.Location = new System.Drawing.Point(140, 180);
+            this.rb_Jupiter.Name = "rb_Jupiter";
+            this.rb_Jupiter.Size = new System.Drawing.Size(75, 24);
+            this.rb_Jupiter.TabIndex = 23;
+            this.rb_Jupiter.TabStop = true;
+            this.rb_Jupiter.Text = "Júpiter";
+            this.rb_Jupiter.UseVisualStyleBackColor = true;
+            // 
+            // rb_Venus
+            // 
+            this.rb_Venus.AutoSize = true;
+            this.rb_Venus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rb_Venus.Location = new System.Drawing.Point(47, 180);
+            this.rb_Venus.Name = "rb_Venus";
+            this.rb_Venus.Size = new System.Drawing.Size(73, 24);
+            this.rb_Venus.TabIndex = 24;
+            this.rb_Venus.TabStop = true;
+            this.rb_Venus.Text = "Vênus";
+            this.rb_Venus.UseVisualStyleBackColor = true;
+            // 
+            // rb_Saturno
+            // 
+            this.rb_Saturno.AutoSize = true;
+            this.rb_Saturno.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rb_Saturno.Location = new System.Drawing.Point(229, 150);
+            this.rb_Saturno.Name = "rb_Saturno";
+            this.rb_Saturno.Size = new System.Drawing.Size(84, 24);
+            this.rb_Saturno.TabIndex = 25;
+            this.rb_Saturno.TabStop = true;
+            this.rb_Saturno.Text = "Saturno";
+            this.rb_Saturno.UseVisualStyleBackColor = true;
+            // 
+            // rb_Marte
+            // 
+            this.rb_Marte.AutoSize = true;
+            this.rb_Marte.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rb_Marte.Location = new System.Drawing.Point(140, 150);
+            this.rb_Marte.Name = "rb_Marte";
+            this.rb_Marte.Size = new System.Drawing.Size(68, 24);
+            this.rb_Marte.TabIndex = 26;
+            this.rb_Marte.TabStop = true;
+            this.rb_Marte.Text = "Marte";
+            this.rb_Marte.UseVisualStyleBackColor = true;
+            // 
+            // rb_Mercurio
+            // 
+            this.rb_Mercurio.AutoSize = true;
+            this.rb_Mercurio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rb_Mercurio.Location = new System.Drawing.Point(46, 150);
+            this.rb_Mercurio.Name = "rb_Mercurio";
+            this.rb_Mercurio.Size = new System.Drawing.Size(88, 24);
+            this.rb_Mercurio.TabIndex = 28;
+            this.rb_Mercurio.TabStop = true;
+            this.rb_Mercurio.Text = "Mercúrio";
+            this.rb_Mercurio.UseVisualStyleBackColor = true;
+            // 
+            // txb_Peso
+            // 
+            this.txb_Peso.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txb_Peso.Location = new System.Drawing.Point(47, 63);
+            this.txb_Peso.Name = "txb_Peso";
+            this.txb_Peso.Size = new System.Drawing.Size(182, 26);
+            this.txb_Peso.TabIndex = 21;
+            // 
+            // bt_Menu
+            // 
+            this.bt_Menu.Location = new System.Drawing.Point(311, 246);
+            this.bt_Menu.Name = "bt_Menu";
+            this.bt_Menu.Size = new System.Drawing.Size(75, 23);
+            this.bt_Menu.TabIndex = 20;
+            this.bt_Menu.Text = "Menu";
+            this.bt_Menu.UseVisualStyleBackColor = true;
+            // 
+            // bt_Calcular
+            // 
+            this.bt_Calcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_Calcular.Location = new System.Drawing.Point(243, 63);
+            this.bt_Calcular.Name = "bt_Calcular";
+            this.bt_Calcular.Size = new System.Drawing.Size(120, 26);
+            this.bt_Calcular.TabIndex = 19;
+            this.bt_Calcular.Text = "Calcular Peso";
+            this.bt_Calcular.UseVisualStyleBackColor = true;
+            this.bt_Calcular.Click += new System.EventHandler(this.bt_Calcular_Click);
+            // 
+            // lb_Planeta
+            // 
+            this.lb_Planeta.AutoSize = true;
+            this.lb_Planeta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_Planeta.Location = new System.Drawing.Point(43, 118);
+            this.lb_Planeta.Name = "lb_Planeta";
+            this.lb_Planeta.Size = new System.Drawing.Size(159, 20);
+            this.lb_Planeta.TabIndex = 17;
+            this.lb_Planeta.Text = "Escolha o Planeta:";
+            // 
+            // lb_Peso
+            // 
+            this.lb_Peso.AutoSize = true;
+            this.lb_Peso.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_Peso.Location = new System.Drawing.Point(41, 40);
+            this.lb_Peso.Name = "lb_Peso";
+            this.lb_Peso.Size = new System.Drawing.Size(162, 20);
+            this.lb_Peso.TabIndex = 18;
+            this.lb_Peso.Text = "Peso na Terra (kg):";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 281);
+            this.Controls.Add(this.rb_Urano);
+            this.Controls.Add(this.rb_Jupiter);
+            this.Controls.Add(this.rb_Venus);
+            this.Controls.Add(this.rb_Saturno);
+            this.Controls.Add(this.rb_Marte);
+            this.Controls.Add(this.rb_Mercurio);
+            this.Controls.Add(this.txb_Peso);
+            this.Controls.Add(this.bt_Menu);
+            this.Controls.Add(this.bt_Calcular);
+            this.Controls.Add(this.lb_Planeta);
+            this.Controls.Add(this.lb_Peso);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rb_Urano;
+        private System.Windows.Forms.RadioButton rb_Jupiter;
+        private System.Windows.Forms.RadioButton rb_Venus;
+        private System.Windows.Forms.RadioButton rb_Saturno;
+        private System.Windows.Forms.RadioButton rb_Marte;
+        private System.Windows.Forms.RadioButton rb_Mercurio;
+        private System.Windows.Forms.TextBox txb_Peso;
+        private System.Windows.Forms.Button bt_Menu;
+        private System.Windows.Forms.Button bt_Calcular;
+        private System.Windows.Forms.Label lb_Planeta;
+        private System.Windows.Forms.Label lb_Peso;
+    }
+}
+
---
+++ Calculadora Interplanetária/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Atividade_Prática
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bt_Calcular_Click(object sender, EventArgs e)
+        {
+           //Entradas
+
+            double peso, gravidade = 1, total;
+            string planeta = "na Terra";
+
+            peso = double.Parse(txb_Peso.Text);
+
+
+            if (rb_Mercurio.Checked)
+            {
+                gravidade = 0.37;
+                planeta = "em Mercúrio";
+            }
+
+            else if (rb_Venus.Checked)
+            {
+                gravidade = 0.88;
+                planeta = "em Vênus";
+            }
+
+            else if (rb_Marte.Checked)
+            {
+                gravidade = 0.38;
+                planeta = "em Marte";
+            }
+            else if (rb_Jupiter.Checked)
+            {
+                gravidade = 2.64;
+                planeta = "em Júpiter";
+            }
+            else if (rb_Saturno.Checked)
+            {
+                gravidade = 1.15;
+                planeta = "em Saturno";
+            }
+            else if (rb_Urano.Checked)
+            {
+                gravidade = 1.17;
+                planeta = "em Urano";
+            }
+            else
+            {
+                //Caixa de mensagem onde informa erro caso a pessoa escolha apenas o peso sem informar o planeta.
+                MessageBox.Show("Por favor selecione um planeta. ", "Erro", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+                Application.Restart();
+            }
+
+            //Processamento
+           total = peso * gravidade;
+
+            //Saída - Caixa de mensagem onde informa o planeta escolhido e o peso convertido.
+            MessageBox.Show("Seu Peso " + planeta + " seria de: " + total + "kg.", "Peso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
